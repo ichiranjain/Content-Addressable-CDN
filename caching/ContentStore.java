66d468b8d57e7f41899bcb113dfbc8006e181856
rushabhmehta91 <rushabhmehta91@hotmail.com>
2015-04-19 22:09:01 -0400

    commit after reviewing

--- caching/ContentStore.java
+++ caching/ContentStore.java
@@ -33,8 +33,12 @@ public class ContentStore {
         return null;
     }
 
+    private ContentPacket forwardingContent(ContentPacket packet) {
+        return new ContentPacket(1, packet);
+    }
+
     /**
-     * Checks for content which is requested if available returns back the content in Content Packet and then updates
+     * Checks for content which is requested if available returns back the content in Content P acket and then updates
      * the score on interfaces. Depending on scores Copy and Delete functions are called.
      *
      * @param incomingPacket - incoming packet
@@ -46,19 +50,26 @@ public class ContentStore {
         String fileName = (String) incomingPacket.getData();
         if (store.containsKey(fileName)) {
             try {
-                updateScoreOnIterface(store.get(fileName), interfaceId);
+                //place content returned
+                ContentPacket packet = updateScoreOnIterface(store.get(fileName), interfaceId); //packet type : 2 = incoming packet
+                if (packet != null) {
+                    return packet;
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            return replyContentRequest(fileName);
+            return replyContentRequest(fileName);//packet type : 1 = reply
         } else {
             return null;
         }
     }
 
     /**
+     * CLIENT SIDE: Elliot
      * When incoming packet is a reply then get the content and return is to the client
+     *
      * @param packet - incoming packet
+     *
      * @return
      */
     private Content incomingReplyContent(ContentPacket packet) {
@@ -68,6 +79,7 @@ public class ContentStore {
     /**
      * When incoming packet have content which to be stored in content store than check the size of the the store
      * if store has required size then place it in store else replace.
+     *
      * @param packet - incoming packet
      * @return
      */
@@ -128,8 +140,9 @@ public class ContentStore {
     /**
      * Update N score of the interface and check for interface score score if it is zero than initiale copy and delete
      * depending to N score on rest all interface
+     *
      * @param contentStoreCopy - content in content store
-     * @param interfaceId - interface Id on which content is requested
+     * @param interfaceId      - interface Id on which content is requested
      * @return
      * @throws Exception
      */
@@ -141,29 +154,30 @@ public class ContentStore {
         }
         boolean copyFlag = false;
         boolean deleteFlag = true;
-        for (Integer index : contentStoreCopy.listofScoreOnInterfaces.keySet()) {
-            if (contentStoreCopy.listofScoreOnInterfaces.get(index) == 0) {
-                copyFlag = true;
-            } else {
+        if (contentStoreCopy.listofScoreOnInterfaces.get(interfaceId) == 0) {
+            copyFlag = true;
+            for (Integer index : contentStoreCopy.listofScoreOnInterfaces.keySet()) {
                 if (contentStoreCopy.listofScoreOnInterfaces.get(index) < contentStoreCopy.getMaxNScore() / 2) {
                     deleteFlag = false;
                 }
             }
-        }
-
-        if (copyFlag) {
-            return copyContent(contentStoreCopy);
         }
         if (copyFlag && deleteFlag) {
             if (!deleteContent(contentStoreCopy)) {
                 throw new Exception("unable to delete content");
             }
         }
+
+        if (copyFlag) {
+            return copyContent(contentStoreCopy);
+        }
+
         return null;
     }
 
     /**
      * send content in the form of content packet
+     *
      * @param content - content requested
      * @return
      */
@@ -173,6 +187,7 @@ public class ContentStore {
 
     /**
      * delete content from current content store
+     *
      * @param content - content requested
      * @return
      */
