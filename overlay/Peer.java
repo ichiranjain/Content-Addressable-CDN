0479699c40fee0165b6764a2a25864d818cddfa4
rushabhmehta91 <rushabhmehta91@hotmail.com>
2015-05-10 21:39:36 -0400

    changing visibility of IP and Neighbours to public so that they are visible in content store

--- overlay/Peer.java
+++ overlay/Peer.java
@@ -1,5 +1,8 @@
 package overlay;
 
+import topology.MainEntryPoint;
+import topology.PassToRoutingLayer;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -7,16 +10,8 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Scanner;
-
-import topology.MainEntryPoint;
-import topology.PassToRoutingLayer;
 
 /**
  * Peer class to represent a node in the overlay network. Implements the
@@ -26,17 +21,16 @@ import topology.PassToRoutingLayer;
  *
  */
 public class Peer { // implements PeerInterface
-	// socket to communicate with neighboring nodes
+    // IP of this node
+    public static String IP;
+    // map of neighboring cacheServers
+    public static HashMap<String, SocketContainer> neighbors;
+    // socket to communicate with neighboring nodes
 	static Socket peerSocket;
-
 	// ID of this node
 	static String ID;
-	// IP of this node
-	static String IP;
 	// serverSocket to listen on
 	static ServerSocket serverSocket;
-	// map of neighboring cacheServers
-	static HashMap<String, SocketContainer> neighbors;
 	// map of neighboring clients and servers
 	static HashMap<String, SocketContainer> clientServers;
 	// map of vacancies with current and neighboring nodes
@@ -385,261 +379,260 @@ public class Peer { // implements PeerInterface
 		System.out.println("Total neighbors notified: " + i);
 	}
 
-	/**
-	 * Method that starts a new thread to begin listening for new nodes that
-	 * wish to join in.
-	 */
-	public void listen() {
-		Listen listen = new Listen(this);
-		listen.start();
-	}
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static Message<JoinPacket> join(String peer)
+            throws IOException,
+            ClassNotFoundException, InterruptedException {
+        long joinStartTime = System.currentTimeMillis();
+        allNodes.add(getIP(IP));
+        peerSocket = new Socket(peer, 43125);
 
-	public void start() throws IOException {
-		System.out.println("Starting node...");
-		System.out.println("IP: " + IP);
-		System.out.println("Waiting for client peer...");
-		allNodes.add(getIP(IP));
-	}
+        JoinPacket packet = new JoinPacket();
+        Message<JoinPacket> joinMessage = new Message<JoinPacket>(1, packet);
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static Message<JoinPacket> join(String peer)
-			throws IOException,
-			ClassNotFoundException, InterruptedException {
-		long joinStartTime = System.currentTimeMillis();
-		allNodes.add(getIP(IP));
-		peerSocket = new Socket(peer, 43125);
+        ObjectOutputStream oos = new ObjectOutputStream(
+                peerSocket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(
+                peerSocket.getInputStream());
 
-		JoinPacket packet = new JoinPacket();
-		Message<JoinPacket> joinMessage = new Message<JoinPacket>(1, packet);
+        oos.writeObject(joinMessage);
+        oos.flush();
+        System.out.println("Join message sent");
+        System.out.println("Waiting for acknowledgement");
+        Message<JoinPacket> mAck = (Message) ois.readObject();
+        long joinStartFinish = System.currentTimeMillis();
 
-		ObjectOutputStream oos = new ObjectOutputStream(
-				peerSocket.getOutputStream());
-		ObjectInputStream ois = new ObjectInputStream(
-				peerSocket.getInputStream());
+        System.out.println("Acknowledgement type: " + mAck.type);
 
-		oos.writeObject(joinMessage);
-		oos.flush();
-		System.out.println("Join message sent");
-		System.out.println("Waiting for acknowledgement");
-		Message<JoinPacket> mAck = (Message) ois.readObject();
-		long joinStartFinish = System.currentTimeMillis();
+        // start listening to connected peer for any future communication
+        Link link = new Link(peerSocket.getRemoteSocketAddress() + "", ois, 3);
+        link.start();
+        //
+        addPeer(mAck.packet, peerSocket, oos, ois, link);
+        // System.out.println("all links up.. now contacting neighbors");
+        // updateNeighbors(connectedTo, m.packet, 50);
 
-		System.out.println("Acknowledgement type: " + mAck.type);
+        // INFORM ROUTING LAYER ABOUT NEW NEIGHBOR
+        System.out.println(System.currentTimeMillis() + " sent to routing:: "
+                + generateID(peerSocket.getRemoteSocketAddress().toString())
+                + " cost::" + (joinStartFinish - joinStartTime));
+        routing.addLink(generateID(peerSocket.getRemoteSocketAddress()
+                .toString()) + "", (int) (joinStartFinish - joinStartTime));
 
-		// start listening to connected peer for any future communication
-		Link link = new Link(peerSocket.getRemoteSocketAddress() + "", ois, 3);
-		link.start();
-		//
-		addPeer(mAck.packet, peerSocket, oos, ois, link);
-		// System.out.println("all links up.. now contacting neighbors");
-		// updateNeighbors(connectedTo, m.packet, 50);
-
-		// INFORM ROUTING LAYER ABOUT NEW NEIGHBOR
-		System.out.println(System.currentTimeMillis() + " sent to routing:: "
-				+ generateID(peerSocket.getRemoteSocketAddress().toString())
-				+ " cost::" + (joinStartFinish - joinStartTime));
-		routing.addLink(generateID(peerSocket.getRemoteSocketAddress()
-				.toString()) + "", (int) (joinStartFinish - joinStartTime));
-
-		return mAck;
-	}
+        return mAck;
+    }
 
 	/**
-	 * Update meta-data of node using received join packet.
-	 * 
-	 * @param m
-	 */
-	public static void updateMetaData(Message<JoinPacket> m) {
-		System.out.println("** Updating meta data - start**");
-		JoinPacket packet = m.packet;
-		System.out.println("Packet allNodes: " + packet.allNodes);
-		System.out.println("Packet neighbors: " + packet.neighbors);
-		// allNodes.addAll(packet.allNodes);
-		// vacancies.putAll(packet.vacancies);
-		System.out.println("After update");
-		System.out.println("allNodes: " + allNodes);
-		System.out.println("neighbors: " + neighbors);
-		System.out.println("** Updating meta data - finish**");
-	}
+     * Update meta-data of node using received join packet.
+     *
+     * @param m
+     */
+    public static void updateMetaData(Message<JoinPacket> m) {
+        System.out.println("** Updating meta data - start**");
+        JoinPacket packet = m.packet;
+        System.out.println("Packet allNodes: " + packet.allNodes);
+        System.out.println("Packet neighbors: " + packet.neighbors);
+        // allNodes.addAll(packet.allNodes);
+        // vacancies.putAll(packet.vacancies);
+        System.out.println("After update");
+        System.out.println("allNodes: " + allNodes);
+        System.out.println("neighbors: " + neighbors);
+        System.out.println("** Updating meta data - finish**");
+    }
 
 	/**
-	 * Method to be called by upper layers to send a message to a particular<br/>
-	 * neighbor.<br/>
-	 * 
-	 * Message type should be set to 7.
-	 * 
-	 * @param ID
-	 * @param m
+     * Method to be called by upper layers to send a message to a particular<br/>
+     * neighbor.<br/>
+     *
+     * Message type should be set to 7.
+     *
+     * @param ID
+     * @param m
+     * @return
+     */
+    @SuppressWarnings("rawtypes")
+    public static boolean sendMessage(String ID, Message m) {
+        synchronized (neighbors.get(idIPMap.get(ID))) {
+            try {
+                // System.out.println(":::ID::: " + ID);
+                SocketContainer sc = neighbors.get(idIPMap.get(ID));
+                if (sc == null) {
+                    sc = clientServers.get(idIPMap.get(ID));
+                }
+                if (sc != null) {
+                    sc.oos.writeObject(m);
+                } else {
+                    System.out.println("Message not sent.. neighbor with ID: " + ID
+                            + "not found.");
+                }
+            } catch (IOException e) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static boolean sendMessageX(String IP, Message m) {
+        try {
+            // System.out.println("SendMessageX::" + IP + " looking in "
+            // + neighbors.keySet());
+            synchronized (neighbors.get(IP)) {
+                SocketContainer sc = neighbors.get(IP);
+                sc.oos.writeObject(m);
+            }
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+	/**
+     * Method to be called by upper layers to send a message to a list of<br/>
+     * neighbors.<br/>
+     *
+     * Message type should be set to 7.
+     *
+     * @param IDs
+     * @param m
 	 * @return
-	 */
+     * @throws IOException
+     */
 	@SuppressWarnings("rawtypes")
-	public static boolean sendMessage(String ID, Message m) {
-		synchronized (neighbors.get(idIPMap.get(ID))) {
-			try {
-				// System.out.println(":::ID::: " + ID);
-				SocketContainer sc = neighbors.get(idIPMap.get(ID));
-				if (sc == null) {
-					sc = clientServers.get(idIPMap.get(ID));
-				}
-				if (sc != null) {
-					sc.oos.writeObject(m);
-				} else {
-					System.out.println("Message not sent.. neighbor with ID: " + ID
-							+ "not found.");
-				}
-			} catch (IOException e) {
-				return false;
-			}
-			return true;
-		}
-	}
-
-	@SuppressWarnings("rawtypes")
-	public static boolean sendMessageX(String IP, Message m) {
-		try {
-			// System.out.println("SendMessageX::" + IP + " looking in "
-			// + neighbors.keySet());
-			synchronized (neighbors.get(IP)) {
-				SocketContainer sc = neighbors.get(IP);
-				sc.oos.writeObject(m);
-			}
-		} catch (IOException e) {
-			return false;
-		}
-		return true;
-	}
-
-	/**
-	 * Method to be called by upper layers to send a message to a list of<br/>
-	 * neighbors.<br/>
-	 * 
-	 * Message type should be set to 7.
-	 * 
-	 * @param IDs
-	 * @param m
-	 * @return
-	 * @throws IOException
-	 */
-	@SuppressWarnings("rawtypes")
-	public static boolean sendMessage(List<String> IDs, Message m)
-			throws IOException {
-		for (String id : IDs) {
-			if (!sendMessage(id, m)) {
-				return false;
+    public static boolean sendMessage(List<String> IDs, Message m)
+            throws IOException {
+        for (String id : IDs) {
+            if (!sendMessage(id, m)) {
+                return false;
 			}
 		}
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Method to be called by upper layers to send a message to all<br/>
-	 * neighbors except ID.<br/>
-	 * 
-	 * Message type should be set to 7.
-	 * 
-	 * @param ID
-	 * @param m
-	 * @return
-	 * @throws IOException
-	 */
-	@SuppressWarnings("rawtypes")
-	public static boolean sendMessageToAllBut(String ID, Message m)
-			throws IOException {
-		List<String> IPs = new ArrayList<String>(neighbors.keySet());
-		for (String ip : IPs) {
-			if (!("" + idIPMap.get(ID)).equals(ip)) {
-				if (!sendMessage(generateID(ip) + "", m)) {
-					return false;
+    /**
+     * Method to be called by upper layers to send a message to all<br/>
+     * neighbors except ID.<br/>
+     * <p/>
+     * Message type should be set to 7.
+     *
+     * @param ID
+     * @param m
+     * @return
+     * @throws IOException
+     */
+    @SuppressWarnings("rawtypes")
+    public static boolean sendMessageToAllBut(String ID, Message m)
+            throws IOException {
+        List<String> IPs = new ArrayList<String>(neighbors.keySet());
+        for (String ip : IPs) {
+            if (!("" + idIPMap.get(ID)).equals(ip)) {
+                if (!sendMessage(generateID(ip) + "", m)) {
+                    return false;
 				}
 			}
 		}
 		return true;
-	}
+    }
 
-	@SuppressWarnings("rawtypes")
-	public static boolean sendMessageToAllButX(String IP, Message m)
-			throws IOException {
-		List<String> IPs = new ArrayList<String>(neighbors.keySet());
-		for (String ip : IPs) {
-			if (!("" + IP).equals(ip)) {
-				if (!sendMessageX(ip, m)) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    @SuppressWarnings("rawtypes")
+    public static boolean sendMessageToAllButX(String IP, Message m)
+            throws IOException {
+        List<String> IPs = new ArrayList<String>(neighbors.keySet());
+        for (String ip : IPs) {
+            if (!("" + IP).equals(ip)) {
+                if (!sendMessageX(ip, m)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-	/**
-	 * Update the number of required neighbors
-	 */
-	public static void updateLogN() {
-		logN = (int) Math.ceil(Math.log10(allNodes.size()) / Math.log10(2));
-	}
-
-
-	/**
-	 * Node ID generator
-	 * 
-	 * @return
-	 * @throws UnknownHostException
-	 */
-	public static long generateID(String IP) throws UnknownHostException {
-		String hostAddress = InetAddress.getLocalHost().getHostAddress();
-		if (!IP.equals("")) {
-			hostAddress = IP;
-		}
-		hostAddress = getIP(hostAddress);
-		// System.out.println("Generating ID... (" + hostAddress + ")");
-		long prime1 = 105137;
-		long prime2 = 179422891;
-		long ID = 0;
-		for (int i = 0; i < hostAddress.length(); i++) {
-			char c = hostAddress.charAt(i);
-			if (c != '.') {
-				ID += (prime1 * (hostAddress.charAt(i) * i)) % prime2;
-			}
-		}
-		// System.out.println("ID: " + ID);
-		idIPMap.put(ID + "", hostAddress);
-
-		return ID;
+    /**
+     * Update the number of required neighbors
+     */
+    public static void updateLogN() {
+        logN = (int) Math.ceil(Math.log10(allNodes.size()) / Math.log10(2));
 	}
 
 	/**
-	 * Checks if new join request can be processed by current node.<br/>
-	 * Does this by checking if number of links after the node joins in are<br/>
-	 * within limits of log<i>n</i>.
-	 * 
-	 * @param peerSocket
-	 * @return
-	 */
-	public static boolean nodeDropRequired() {
-		int newNetworkSize = Peer.allNodes.size();
-		int presentNeighbors = Peer.neighbors.size();
-		System.out.println("newNetworkSize: " + newNetworkSize);
-		System.out.println("presentNeighbors: " + presentNeighbors);
-		int requiredNeighbors = (int) Math.ceil(Math.log(newNetworkSize)
-				/ Math.log(2));
-		System.out.println("Neighbors present: " + presentNeighbors
-				+ "\nNeighbors required: " + requiredNeighbors);
-		if (presentNeighbors > requiredNeighbors) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+     * Node ID generator
+     *
+     * @return
+     * @throws UnknownHostException
+     */
+    public static long generateID(String IP) throws UnknownHostException {
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+        if (!IP.equals("")) {
+            hostAddress = IP;
+        }
+        hostAddress = getIP(hostAddress);
+        // System.out.println("Generating ID... (" + hostAddress + ")");
+        long prime1 = 105137;
+        long prime2 = 179422891;
+        long ID = 0;
+        for (int i = 0; i < hostAddress.length(); i++) {
+            char c = hostAddress.charAt(i);
+            if (c != '.') {
+                ID += (prime1 * (hostAddress.charAt(i) * i)) % prime2;
+            }
+        }
+        // System.out.println("ID: " + ID);
+        idIPMap.put(ID + "", hostAddress);
 
-	/**
-	 * This method checks if the number of links on the node are correct
-	 * according to the total number of nodes in the network
-	 */
-	public static boolean linksSatisfied() {
-		int linksPresent = neighbors.size();
-		int linksRequired = (int) Math.ceil(Math.log(allNodes.size())
-				/ Math.log(2));
-		System.out.println("linksPresent: " + linksPresent);
-		System.out.println("linksRequired: " + linksRequired);
-		return linksPresent == linksRequired;
+        return ID;
+    }
+
+    /**
+     * Checks if new join request can be processed by current node.<br/>
+     * Does this by checking if number of links after the node joins in are<br/>
+     * within limits of log<i>n</i>.
+     *
+     * @param peerSocket
+     * @return
+     */
+    public static boolean nodeDropRequired() {
+        int newNetworkSize = Peer.allNodes.size();
+        int presentNeighbors = Peer.neighbors.size();
+        System.out.println("newNetworkSize: " + newNetworkSize);
+        System.out.println("presentNeighbors: " + presentNeighbors);
+        int requiredNeighbors = (int) Math.ceil(Math.log(newNetworkSize)
+                / Math.log(2));
+        System.out.println("Neighbors present: " + presentNeighbors
+                + "\nNeighbors required: " + requiredNeighbors);
+        if (presentNeighbors > requiredNeighbors) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * This method checks if the number of links on the node are correct
+     * according to the total number of nodes in the network
+     */
+    public static boolean linksSatisfied() {
+        int linksPresent = neighbors.size();
+        int linksRequired = (int) Math.ceil(Math.log(allNodes.size())
+                / Math.log(2));
+        System.out.println("linksPresent: " + linksPresent);
+        System.out.println("linksRequired: " + linksRequired);
+        return linksPresent == linksRequired;
+    }
+
+    /**
+     * Method that starts a new thread to begin listening for new nodes that
+     * wish to join in.
+     */
+    public void listen() {
+        Listen listen = new Listen(this);
+        listen.start();
+    }
+
+    public void start() throws IOException {
+        System.out.println("Starting node...");
+        System.out.println("IP: " + IP);
+        System.out.println("Waiting for client peer...");
+        allNodes.add(getIP(IP));
 	}
 }
