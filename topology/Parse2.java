f4f2b4a24a17170cc51e60b2e4761b762efc5b1a
Elliott Fodi <elfodi@gmail.com>
2015-05-18 23:14:01 -0400

    deleted unused classes

--- topology/Parse2.java
+++ topology/Parse2.java
@@ -2,15 +2,11 @@ package topology;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-import packetObjects.AddNodeObj;
 import packetObjects.DataObj;
 import packetObjects.IntrestObj;
 import packetObjects.LinkObj;
-import packetObjects.ModifyNodeObj;
-import packetObjects.NeighborRequestObj;
 import packetObjects.PrefixListObj;
 import packetObjects.PrefixObj;
-import packetObjects.RemoveNodeObj;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -26,58 +22,8 @@ public class Parse2 {
 
 	}
 
-	public AddNodeObj parseAddNodeJson(JsonObject jsonObject, String originalPacket) throws Exception{
-
-		JsonElement jsonNameElement = jsonObject.get("nodeName");
-		String nodeName = jsonNameElement.getAsString();
 
 
-		JsonElement jsonIDElement = jsonObject.get("msgID");
-		String msgID = jsonIDElement.getAsString();
-
-
-		JsonElement jsonNeighborsElement = jsonObject.get("neighbors");
-		String neighborsString = jsonNeighborsElement.getAsString();
-		Type neighborsType = new TypeToken<ArrayList<NeighborAndCostStrings>>(){}.getType();
-		ArrayList<NeighborAndCostStrings> neighborsList = gson.fromJson(neighborsString, neighborsType);
-
-
-		AddNodeObj addNodeInfo = new AddNodeObj(nodeName, neighborsList, msgID, originalPacket);
-
-		return addNodeInfo;
-	}
-
-	public ModifyNodeObj parseModifyNodeJson(JsonObject jsonObject, String originalPacket) throws Exception{
-
-		//JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
-		JsonElement jsonNameElement = jsonObject.get("nodeName");
-		String nodeName = jsonNameElement.getAsString();
-
-		JsonElement jsonIDElement = jsonObject.get("msgID");
-		String msgID = jsonIDElement.getAsString();
-
-		JsonElement jsonNeighborsElement = jsonObject.get("neighbors");
-		String neighborsString = jsonNeighborsElement.getAsString();
-		Type neighborsType = new TypeToken<ArrayList<NeighborAndCostStrings>>(){}.getType();
-		ArrayList<NeighborAndCostStrings> neighborsList = gson.fromJson(neighborsString, neighborsType);
-
-
-		ModifyNodeObj modifyNodeInfo = new ModifyNodeObj(nodeName, neighborsList, msgID, originalPacket);
-		return modifyNodeInfo;
-	}
-
-	public RemoveNodeObj parseRemoveNodeJson(JsonObject jsonObject, String originalPacket) throws Exception{
-
-		//JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
-		JsonElement jsonNameElement = jsonObject.get("removeNodeName");
-		String removeNodeName = jsonNameElement.getAsString();
-
-		JsonElement jsonIDElement = jsonObject.get("msgID");
-		String msgID = jsonIDElement.getAsString();
-
-		RemoveNodeObj removeNodeInfo = new RemoveNodeObj(removeNodeName, msgID, originalPacket);
-		return removeNodeInfo;
-	}
 
 	public PrefixObj parsePrefixJson(JsonObject jsonObject, String originalPacket) throws Exception{
 
@@ -223,14 +169,5 @@ public class Parse2 {
 		return new LinkObj(nodeName, cost);
 	}
 
-	public NeighborRequestObj parseRequestNeighbors(JsonObject jsonObject) throws Exception{
-
-		//JsonObject jsonObject = gson.fromJson(jsonString, JsonObject.class);
-
-		JsonElement jsonNodeNameElement = jsonObject.get("nodeName");
-		String nodeName = jsonNodeNameElement.getAsString();
-
-		return new NeighborRequestObj(nodeName);
-	}
 
 }
